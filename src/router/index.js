d7a8ab9e5e821cd76dc3f30c17b708dec5a6d6ec
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-22 18:17:32 +0800

    完成路由的导入和登录页面的创建

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue"
+
+import login from "../view/login/login.vue"
+
+// 导入 vue-router
+import VueRouter from 'vue-router'
+//注册 vue-router
+Vue.use(VueRouter)
+// 实例化
+const router = new VueRouter({
+//这里就是路由的配制项
+routes: [
+ {
+path: '/login',
+component:login  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+ },
+ {
+path: '*',  //路由重定向
+redirect: '/', //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+ }
+  ]
+})
+export default router
