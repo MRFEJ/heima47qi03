11b60bca2bd5c9d0df76cd88b896f4ca5cd987e4
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-22 19:52:52 +0800

    完成注册表单的表单验证

--- src/view/login/components/reg.vue
+++ src/view/login/components/reg.vue
@@ -17,10 +17,10 @@
         <el-input v-model="form.password" autocomplete="off"></el-input>
       </el-form-item>
 
-      <el-form-item label="图形码" :label-width="formLabelWidth">
+      <el-form-item label="图形码" :label-width="formLabelWidth" prop="code">
         <el-row>
           <el-col :span="17">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
+            <el-input v-model="form.code" autocomplete="off"></el-input>
           </el-col>
           <el-col :span="6" :offset="1">
             <img src="../img/login_logo.png" alt />
@@ -28,10 +28,10 @@
         </el-row>
       </el-form-item>
 
-      <el-form-item label="验证码" :label-width="formLabelWidth">
+      <el-form-item label="验证码" :label-width="formLabelWidth" prop="rcode">
         <el-row>
           <el-col :span="17">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
+            <el-input v-model="form.rcode" autocomplete="off"></el-input>
           </el-col>
           <el-col :span="6" :offset="1">
             <el-button>获取用户验证码</el-button>
@@ -41,7 +41,7 @@
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button @click="dialogFormVisible = false">取 消</el-button>
-      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      <el-button type="primary" @click="sure">确 定</el-button>
     </div>
   </el-dialog>
 </template>
@@ -56,11 +56,46 @@ export default {
         name: "",
         email: "",
         phone: "",
-        password: ""
+        password: "",
+        code:'',
+        rcode:''
       },
-      rules: {}
+      rules: {
+        name: [{ required: true, message: "昵称不能留空", trigger: "blur" }],
+        email: [
+          { required: true, message: "邮箱不能留空", trigger: "blur" },
+          {
+            pattern: /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/,
+            message: "邮箱格式不正确",
+            trigger: "blur"
+          }
+        ],
+        phone: [
+          { required: true, message: "手机号不能留空", trigger: "blur" },
+          {
+            pattern: /0?(13|14|15|18)[0-9]{9}/,
+            message: "手机号格式不正确",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "密码不能留空", trigger: "blur" },
+          { max: 12, min: 6, message: "密码格式不正确", trigger: "change" }
+        ],
+        code:[{ required: true, message: "图像码不能留空", trigger: "blur" }],
+        rcode:[{ required: true, message: "验证码不能留空", trigger: "blur" }]
+      }
     };
-  }
+  },
+  methods: {
+      sure(){
+          this.$refs.form.validate(v=>{
+              if(v){
+                  alert('注册成功')
+              }
+          })
+      }
+  },
 };
 </script>
 
