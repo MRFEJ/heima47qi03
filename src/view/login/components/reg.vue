731d2ce91c7eecd70eb6dddf9d252c4ac633d356
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-22 19:43:35 +0800

    完成注册模块的基本布局

--- src/view/login/components/reg.vue
+++ src/view/login/components/reg.vue
@@ -1,8 +1,42 @@
 <template>
-  <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
-    <el-form :model="form">
-      <el-form-item label="活动名称" :label-width="formLabelWidth">
+  <el-dialog title="用户注册" center :visible.sync="dialogFormVisible">
+    <el-form :model="form" :rules="rules">
+      <el-form-item label="昵称" :label-width="formLabelWidth" prop="name">
         <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="邮箱" :label-width="formLabelWidth" prop="email">
+        <el-input v-model="form.email" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="手机" :label-width="formLabelWidth" prop="phone">
+        <el-input v-model="form.phone" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码" :label-width="formLabelWidth" prop="password">
+        <el-input v-model="form.password" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="图形码" :label-width="formLabelWidth">
+        <el-row>
+          <el-col :span="17">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-col>
+          <el-col :span="6" :offset="1">
+            <img src="../img/login_logo.png" alt />
+          </el-col>
+        </el-row>
+      </el-form-item>
+
+      <el-form-item label="验证码" :label-width="formLabelWidth">
+        <el-row>
+          <el-col :span="17">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-col>
+          <el-col :span="6" :offset="1">
+            <el-button>获取用户验证码</el-button>
+          </el-col>
+        </el-row>
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
@@ -17,13 +51,24 @@ export default {
   data() {
     return {
       dialogFormVisible: false,
+      formLabelWidth: "65px",
       form: {
+        name: "",
+        email: "",
+        phone: "",
+        password: ""
       },
-      formLabelWidth: "120px"
+      rules: {}
     };
   }
 };
 </script>
 
-<style>
+<style lang="less">
+.el-dialog__header {
+  background: linear-gradient(to right, #01c5fb, #1394fc);
+  span {
+    color: #fff;
+  }
+}
 </style>
