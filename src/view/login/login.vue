d4c565a929f73981674bed32d4ac133d0b76eda4
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-22 18:33:39 +0800

    路由的导入和完成登录页面的基本构架

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>666</div>
+  <div class="box">
+    <div class="login-left"></div>
+    <img src="./img/login_banner_ele.png" alt />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="less">
+.box {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  display: flex;
+  justify-content:space-around;
+  align-items: center;
+  .login-left {
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+  }
+}
+html,
+body {
+  height: 100%;
+}
 </style>
