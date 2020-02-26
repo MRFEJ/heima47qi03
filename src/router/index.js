faaeb02686f40289681c7a5c3fb324092f5043c7
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-26 13:52:16 +0800

    引入导航守卫,完成路由切换时显示和关闭进度条功能

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ import Question from "@/view/index/Question/Question.vue"
 import Companies from "@/view/index/Companies/Companies.vue"
 import Subject from "@/view/index/Subject/Subject.vue"
 
+// 导入进度条
+import nprogress from "nprogress"
+// 导入进度条的样式
+import "nprogress/nprogress.css"
 // 导入 vue-router
 import VueRouter from 'vue-router'
 //注册 vue-router
@@ -37,5 +41,20 @@ const router = new VueRouter({
     // redirect: '/', //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
     //  }
   ]
+})
+
+// 导入导航守卫
+// 当路由改变前
+router.beforeEach((to, from, next) => {
+  // to and from are both route objects. must call `next`.
+  // 开启进度条
+  nprogress.start()
+  next();
+})
+// 当路由改变后
+router.afterEach(() => {
+  // 关闭进度条
+  nprogress.done();
+  // to and from are both route objects.
 })
 export default router
