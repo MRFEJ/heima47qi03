fe56cecd0a49eb5ae87137a29199954c6c30c5f8
MRFEJ <1403676630@qq.comgit config --global user.name Your>
2020-02-26 14:44:20 +0800

    完成路由元设置给页面标题功能

--- src/router/index.js
+++ src/router/index.js
@@ -32,17 +32,29 @@ const router = new VueRouter({
   routes: [
     {
       path: '/login',
-      component: login  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+      component: login,  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+      meta: { title: "登录" }
     },
     {
       path: "/index",
       component: index,
+      meta: { title: "首页" },
       children: [
-        { path: 'overview', component: overview },
-        { path: 'users', component: users },
-        { path: 'Question', component: Question },
-        { path: 'Companies', component: Companies },
-        { path: 'Subject', component: Subject },
+        {
+          path: 'overview', component: overview, meta: { title: "数据概览" },
+        },
+        {
+          path: 'users', component: users, meta: { title: "用户列表" },
+        },
+        {
+          path: 'Question', component: Question, meta: { title: "题库列表" },
+        },
+        {
+          path: 'Companies', component: Companies, meta: { title: "企业列表" },
+        },
+        {
+          path: 'Subject', component: Subject, meta: { title: "学科列表" },
+        },
       ]
     },
     {
@@ -79,7 +91,9 @@ router.beforeEach((to, from, next) => {
   }
 })
 // 当路由改变后
-router.afterEach(() => {
+router.afterEach((to) => {
+  // 给页面的标题命名
+  document.title = to.meta.title;
   // 关闭进度条
   nprogress.done();
   // to and from are both route objects.
